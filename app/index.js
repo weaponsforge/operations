9673cf9e1850bf577223593a859cb98c3a553b5d
weaponsforge <forgeweapons@gmail.com>
2025-01-24 17:16:55 +0800

    fix: move index.js

---
+++ app/index.js
@@ -0,0 +1,8 @@
+const { add, subtract, multiply, divide } = require('./src/utils')
+
+module.exports = {
+  add,
+  subtract,
+  multiply,
+  divide
+}
--- app/src/index.js
+++
@@ -1,8 +0,0 @@
-const { add, subtract, multiply, divide } = require('./utils')
-
-module.exports = {
-  add,
-  subtract,
-  multiply,
-  divide
-}
